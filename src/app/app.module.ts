c156e9554cbcf72999fa0488fbb7a392d370dd7c
KritanTamr <tamrakar@sheridancollege.ca>
2023-11-13 01:27:58 -0500

    Added Initial Header and Sub-Header Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NoticeBarComponent } from './Notice-Bar/notice-bar.component';
+import { NoticeBarComponent } from './notice-bar/notice-bar.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [AppComponent, NoticeBarComponent, NavbarComponent, HeaderComponent],
+  declarations: [
+    AppComponent,
+    NoticeBarComponent,
+    NavbarComponent,
+    HeaderComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
