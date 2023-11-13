22fa12b3a383e5b5eed57ecb85b6158572631d36
KritanTamr <tamrakar@sheridancollege.ca>
2023-11-13 01:26:06 -0500

    added initial header and sub-components.

---
+++ src/app/notice-bar/notice-bar.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-notice-bar',
+  templateUrl: './notice-bar.component.html',
+  styleUrls: ['./notice-bar.component.scss'],
+})
+export class NoticeBarComponent {}
