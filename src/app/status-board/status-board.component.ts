d7370935afef3fc8f2902ac738011e90412cc962
KritanTamr <tamrakar@sheridancollege.ca>
2023-11-18 03:59:03 -0500

    added a status-board in the project.

--- src/app/status-board/status-board.component.ts
+++ src/app/status-board/status-board.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-status-board',
   templateUrl: './status-board.component.html',
-  styleUrls: ['./status-board.component.scss']
+  styleUrls: ['./status-board.component.scss'],
 })
 export class StatusBoardComponent {
-
+  active_members = 200;
+  sponsors_count = 8;
+  championship_won = 2;
 }
