970813a9b2daebc64095994e8a3e4ef46df628b4
KritanTamr <tamrakar@sheridancollege.ca>
2023-11-23 20:00:34 -0500

    integrated achievements concept.

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.scss']
+  styleUrls: ['./main.component.scss'],
 })
 export class MainComponent {
+  title: String;
 
+  constructor() {
+    this.title = '';
+  }
+
+  protected createTitle(title: String): String {
+    this.title = title;
+    return this.title;
+  }
 }
--- src/app/typographical-headings/typographical-headings.component.ts
+++ src/app/typographical-headings/typographical-headings.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-typographical-headings',
   templateUrl: './typographical-headings.component.html',
-  styleUrls: ['./typographical-headings.component.scss']
+  styleUrls: ['./typographical-headings.component.scss'],
 })
 export class TypographicalHeadingsComponent {
-
+  @Input() title: String = '';
 }
