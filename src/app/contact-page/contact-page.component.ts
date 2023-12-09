814e0b2b3332edb3c034b56792e3940158700f34
KritanTamr <tamrakar@sheridancollege.ca>
2023-12-08 23:59:34 -0500

    user input

--- src/app/contact-page/contact-page.component.ts
+++ src/app/contact-page/contact-page.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-contact-page',
   templateUrl: './contact-page.component.html',
-  styleUrls: ['./contact-page.component.scss']
+  styleUrls: ['./contact-page.component.scss'],
 })
 export class ContactPageComponent {
+  values = '';
 
+  onKey(event: any) {
+    this.values += event.target.value;
+  }
 }
