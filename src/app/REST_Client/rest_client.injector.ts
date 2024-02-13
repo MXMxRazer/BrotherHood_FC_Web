d1fb54252a9e1f5faaecedbff26b0f5ac35973cd
KritanTamr <tamrakar@sheridancollege.ca>
2024-02-12 22:31:33 -0500

    updating...

--- src/app/REST_Client/rest_client.injector.ts
+++ src/app/REST_Client/rest_client.injector.ts
@@ -1,16 +1,15 @@
-// import { HttpClient } from '@angular/common/http';
-// import { Injectable } from '@angular/core';
-// import { Observable, map } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, map } from 'rxjs';
 
-// @Injectable()
-// export class RestService {
-//   constructor(private http: HttpClient) {}
+export class RestService {
+  constructor(private http: HttpClient) {}
 
-//   url: string = 'to be noted! => securely';
+  url: string = 'http://localhost:8080/send-mail-demo';
 
-//   sendEmail(context: any): Observable<any> {
-//     return this.http
-//       .post(this.url, JSON.stringify(context))
-//       .pipe(map((response: any) => response.json()));
-//   }
-// }
+  sendEmail(context: any): Observable<any> {
+    return this.http
+      .post(this.url, JSON.stringify(context))
+      .pipe(map((response: any) => response.json()));
+  }
+}
--- src/app/footer/contact-page/contact-page.component.ts
+++ src/app/footer/contact-page/contact-page.component.ts
@@ -1,40 +1,43 @@
 import { Component, OnInit } from '@angular/core';
 import { Logger } from '../../logger/logger';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
-// import { RestService } from 'src/app/REST_Client/rest_client.injector';
+import { FormControl, FormGroup } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormType } from './contact_form_type/FormType.type';
 
 @Component({
   selector: 'app-contact-page',
   standalone: true,
   templateUrl: './contact-page.component.html',
   styleUrls: ['./contact-page.component.scss'],
-  imports: [HttpClientModule],
+  imports: [HttpClientModule, ReactiveFormsModule],
 })
 export class ContactPageComponent implements OnInit {
-  values = '';
+  url: string = 'http://localhost:8080/send-mail-demo';
+  contactForm: FormGroup;
 
-  constructor(
-    private logger: Logger,
-    private http: HttpClient // private restService: RestService
-  ) {}
+  constructor(private logger: Logger, private http: HttpClient) {}
 
-  ngOnInit(): void {}
-
-  // form actions
-  onKey(event: any): void {
-    this.values += event.target.value;
-    this.logger.log(this.values);
+  ngOnInit(): void {
+    this.contactForm = new FormGroup({
+      first_name: new FormControl('xyz'),
+      last_name: new FormControl(''),
+      email: new FormControl(''),
+      phone: new FormControl(''),
+    });
   }
 
-  // restService contacter in angular (indirectly contacting the backend services for sending mail to the end-user)
-  // sendEmail(): void {
-  //   this.restService.sendEmail({ FormBuilderInformation: 'none' }).subscribe(
-  //     (data) => {
-  //       console.log(data);
-  //     },
-  //     (err) => {
-  //       console.log(err);
-  //     }
-  //   );
-  // }
+  onFormSubmit(form: FormGroup): void {
+    if (form.valid) {
+      this.http
+        .post<FormType>('http://localhost:8080/send-mail', {
+          to: 'tamrakar@sheridancollege.ca',
+          subject: 'fromAngular',
+          message: form.value.first_name + ' ' + form.value.last_name,
+        })
+        .subscribe((data) => {
+          console.log(data);
+        });
+    }
+  }
 }
